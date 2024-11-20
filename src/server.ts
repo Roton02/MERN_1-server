f2aaa55254af185902e1fd9a309c323b52039ee3
Roton02 <rph645102@gmail.com>
2024-11-20 17:23:53 +0600

    create the schema for student

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,6 @@
 import mongoose from 'mongoose';
 import app from './app';
-import config from './app/config';
-const port = 5000;
-
-// index.js
-const a = 1;
+import config from './app/config';;
 
 async function main() {
   await mongoose.connect(config.database_url as string);
